6b419021fd0cca7d45dfef0dcd1dd8482db75d50
angie <angiewheeler99@gmail.com>
2026-09-15 22:09:48 -0600

    Meetings Page error when trying to deploy in vercal, has been fixed

--- app/meetings/page.tsx
+++ app/meetings/page.tsx
@@ -1,8 +1,17 @@
+export const runtime = "nodejs"; //this is a server file!
+
 import MeetingCard from "@/components/MeetingCard";
 import { SacramentMeeting } from "@/lib/types";
+import { headers } from "next/headers";
 
 export default async function MeetingsPage() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/meetings`, {
+  //const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/meetings`, { //vercel was erroring out about this, so I changed it to the below
+  const h = await headers();
+const origin = h.get("host");
+  const protocol = process.env.NODE_ENV === "development" ? "http" : "https";
+
+  const res = await fetch(`${protocol}://${origin}/api/meetings`, {
+    method: "GET",
     cache: "no-store",
   });
 
