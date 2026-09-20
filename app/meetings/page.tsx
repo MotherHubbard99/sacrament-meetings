352f175a14391eca9c9057c4c811774d1f0f4135
angie <angiewheeler99@gmail.com>
2026-09-20 12:57:32 -0600

    Trying to fix the Meeting link on Vercel....again

--- app/meetings/page.tsx
+++ app/meetings/page.tsx
@@ -1,22 +1,14 @@
+export const dynamic = "force-dynamic"  //prevents static generation
 export const runtime = "nodejs"; //this is a server file!
 
 import MeetingCard from "@/components/MeetingCard";
 import { SacramentMeeting } from "@/lib/types";
-import { headers } from "next/headers";
 
-export default async function MeetingsPage() {
-  //const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/meetings`, { //vercel was erroring out about this, so I changed it to the below
-  // const h = await headers();
-  // const origin = h.get("host");
-  // const protocol = process.env.NODE_ENV === "development" ? "http" : "https";
-
-  // const res = await fetch(`${protocol}://${origin}/api/meetings`, {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-
-  const res = await fetch(`${baseUrl}/api/meetings`, {
-   method: "GET",
+  export default async function MeetingsPage() {
+  const res = await fetch("/api/meetings", {
+    method: "GET",
     cache: "no-store",
-   });
+  });
 
   const meetings: SacramentMeeting[] = await res.json();
 
